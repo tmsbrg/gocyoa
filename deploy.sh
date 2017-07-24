31bfa7d478861d0ddeb6c7ebb125c63d25d182a7
Thomas van der Berg <ik@thomasvanderberg.nl>
2017-07-24 21:03:06 +0200

    deploy.sh: restart after deploy, add status info

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,14 @@ if [ $UID -ne 0 ]; then
 fi
 
 # compile
+echo compiling... 1>&2
 go build
 
 # create user
+echo creating user... 1>&2
 adduser --system --no-create-home --gecos '' --shell /bin/false --disabled-login --group gocyoa
+
+echo installing... 1>&2
 
 # create directory
 mkdir -m 775 -p /srv/gocyoa
@@ -20,3 +24,8 @@ install -m 664 *.html /srv/gocyoa
 # install systemd service
 install -m 664 gocyoa.service /etc/systemd/system
 systemctl daemon-reload
+
+echo starting... 1>&2
+systemctl restart gocyoa
+
+echo deployment successful 1>&2
